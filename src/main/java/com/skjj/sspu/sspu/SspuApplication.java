78072c29a784ace993346744290417e2109995cf
LuryN <1242320909@qq.com>
2019-12-31 13:25:11 +0800

    init repo

---
+++ src/main/java/com/skjj/sspu/sspu/SspuApplication.java
@@ -0,0 +1,13 @@
+package com.skjj.sspu.sspu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SspuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SspuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skjj/sspu/sspu/SspuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skjj.sspu.sspu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SspuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
